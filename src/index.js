478288cb793da9eab6b670f6c40eb95929d5e4b1
rupak <rupaksamantainbox@gmail.com>
2024-07-27 21:07:28 +0530

    modify

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const cookieParser = require ('cookie-parser');
 const serverConfig = require('./config/serverConfig');
 const connectDB = require('./config/dbConfig');
-const User = require('./schema/userSchema');
 const userRouter = require('./routes/userRoute');
 const cartRouter = require('./routes/cartRoute');
 const authRouter = require('./routes/authRoute');
-const cookieParser = require ('cookie-parser');
 const { isLoggedIn } = require('./validation.js/authValidator');
+const uploader = require('./middlewares/multerMiddleware');
+const cloudinary = require('./config/cloudinaryConfig');
+const fs = require('fs/promises');
 
 const app = express();
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.text());
@@ -22,6 +25,27 @@ app.post('/ping', isLoggedIn, (req,res) => {
     console.log(req.body)
     console.log(req.cookies)
     return res.json({message : 'ping'})
+})
+
+// app.post('/photo',uploader.single('IncommingFile'), async (req,res)=>  {
+//     console.log(cloudinary.api_key);
+//     try {
+//         const result = await cloudinary.uploader.upload(req.file.path)
+//         console.log(result)
+//         //await fs.unlink(req.file.path)
+//         return res.json({ message : "ok" })
+//     } catch (error) {
+//         return res.json({ message : error })
+//     }
+   
+// })
+
+app.post('/photo', uploader.single('IncomingFile'), async (req, res) => {
+    console.log(req.file);
+    const result = await cloudinary.uploader.upload(req.file.path);
+    console.log("result from cloudinary", result);
+    await fs.unlink(req.file.path);
+    return res.json({message: 'ok'});
 })
 
 app.listen(serverConfig.PORT, async() => {
---
+++ src/middlewares/multerMiddleware.js
@@ -0,0 +1,15 @@
+const multer  = require('multer');
+const path = require('path');
+
+const storage  = multer.diskStorage({ 
+    destination : (req, file, next ) => {
+        next( null, 'uploads/' )
+    },
+    filename : (req, file, next) => {
+        console.log(file)
+        next( null, `${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+const uploader = multer({ storage: storage })
+
+module.exports = uploader;
